6e4f2872d3d92d92fb74cf28fc874cba688ee9d1
Mariusz Walusiak <marwal91@o2.pl>
2021-11-06 09:17:29 +0100

    Else if Statements

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
-var youLikeMeat = true;
-var myNum = 10;
+var myAge = 10;
 
-
-if (myNum == 10){
-
-    document.write("myNum is equal to 10");
-} else {
-    document.write("myNum is not equal to 10");
+if (myAge>39){
+    document.write("You are over 30!")
+} else if ( myAge>20){
+    document.write("You are over 20!")
+} else if (myAge > 10){
+    document.write("You are over 10!")
+} else{
+    document.write("You are not over 10!")
 }
+
