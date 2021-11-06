6d42df2e0c59e7203245908aa378c7d30a36ec56
Mariusz Walusiak <marwal91@o2.pl>
2021-11-06 10:50:30 +0100

    Break & Continue

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
-var links = document.getElementsByTagName("a");
+for (i = 0; i < 10; i++){
 
+    if (i === 5 || i === 3){
 
-for (i = 1 ; i <= links.length; i++){
-    console.log("This is link number" + i)
+        continue;
+    }
+
+    console.log(i)
+
+    if (i === 7){
+        break;
+    }
 }
 
-document.write("all links now looped")
+console.log("I have broken the loop")
