54b61d4753d6efc676b40377ec5822659ef11496
Mariusz Walusiak <marwal91@o2.pl>
2021-11-08 12:07:32 +0100

    JavaScript Timers II

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
-var myMessage = document.getElementById("message")
+var colourChanger = document.getElementById("colour-changer");
+var colours = ["red","blue", "green","pink"];
+var counter = 0;
 
-function showMessage(){
+function changeColour(){
 
-    myMessage.className = "show";
-    
+    if (counter >= colours.length){
+        counter = 0;
+    }
+
+    colourChanger.style.background = colours[counter];
+    counter++;
+   
+
 }
 
-setTimeout(showMessage,3000)
+var myTimer = setInterval(changeColour,3000);
+
+colourChanger.onclick = function(){
+
+    clearInterval(myTimer);
+    colourChanger.innerHTML = "Timer stopped";
+
+}
